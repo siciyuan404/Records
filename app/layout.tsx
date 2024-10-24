7d977037702bdbb62094a3ab0ebfcd9be607b748
mxrain <mxrain@yeah.net>
2024-10-24 13:38:16 +0800

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,13 +5,12 @@ import { Providers } from './components/Providers';
 import { Toaster } from "@/components/ui/toaster"
 import 'nprogress/nprogress.css'
 import ClientLayout from './ClientLayout';  // 添加这行
-import { Metadata } from 'next';
 
 const inter = Inter({ subsets: ['latin'] })
 
-export const metadata: Metadata = {
-  title: '资源桶',
-  description: '资源桶 - 您的资源管理平台',
+export const metadata = {
+  title: '四次元资源桶',
+  description: '四次元资源桶 - 您的资源管理平台',
   viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
 }
 
